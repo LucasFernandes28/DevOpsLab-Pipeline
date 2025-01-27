5ca1a83929f5e85b77c8559c39921d0602710c07
lucasf-devops <lucas-fernandes@DESKTOP-301K1KQ>
2025-01-27 09:25:33 -0300

    Initial Commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/lucas-fernandes/my-website/node_modules/infima/dist/css/default/default.css"),
+  require("/home/lucas-fernandes/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/lucas-fernandes/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/lucas-fernandes/my-website/src/css/custom.css"),
+];
